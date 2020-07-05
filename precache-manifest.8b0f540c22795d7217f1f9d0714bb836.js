aeecb2d8c6245644ee4c6d885f76d233c8eff12a
adeJo <josh_d_a@hotmail.com>
2020-07-05 10:03:31 -0400

    Updates

---
+++ precache-manifest.8b0f540c22795d7217f1f9d0714bb836.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "396bdaefee6642323bd35674a4b976f4",
+    "url": "/Grade-Tracker/index.html"
+  },
+  {
+    "revision": "17aa3eee556d6c966231",
+    "url": "/Grade-Tracker/static/css/main.e26b3ee0.chunk.css"
+  },
+  {
+    "revision": "6f32941b6f6283b26998",
+    "url": "/Grade-Tracker/static/js/2.b9b768b4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Grade-Tracker/static/js/2.b9b768b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17aa3eee556d6c966231",
+    "url": "/Grade-Tracker/static/js/main.27229fa1.chunk.js"
+  },
+  {
+    "revision": "7291aad2fa762dcaff00",
+    "url": "/Grade-Tracker/static/js/runtime-main.0438841f.js"
+  }
+]);
